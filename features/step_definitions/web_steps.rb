955ff862fc0d0b7423c37dd9d4086cdfd62cbbd0
Dan Blakeman <DanBlakeman@users.noreply.github.com>
2015-04-01 22:48:24 +0100

    Board has a key

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -59,7 +59,10 @@ Then(/^I should be on the homepage$/) do
 end
 
 Then(/^i should see the board$/) do
-  table = ((" ~ " * 10) + "\n") * 10
+  table = "   1  2  3  4  5  6  7  8  9  10  \n"
+  ("A".."J").each do |row|
+    table << (row + " " + (" ~ " * 10) + "\n")
+  end
   expect(page).to have_content(table)
 end
 
@@ -69,7 +72,10 @@ When(/^i miss a ship$/) do
 end
 
 Then(/^i should see the updated board$/) do
-  table = ((" ~ " * 10) + "\n") * 10
+  table = "   1  2  3  4  5  6  7  8  9  10  \n"
+  ("A".."J").each do |row|
+    table << (row + " " + (" ~ " * 10) + "\n")
+  end
   table = table[0..-5] + "o \n"
   expect(page).to have_content(table)
 end

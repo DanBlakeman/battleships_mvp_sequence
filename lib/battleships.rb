6c7cef3a33b2658c671651a120889b8014def187
Dan Blakeman <DanBlakeman@users.noreply.github.com>
2015-04-02 23:37:48 +0100

    players can battle remotely

--- lib/battleships.rb
+++ lib/battleships.rb
@@ -5,37 +5,54 @@ require_relative 'destroyer'
 class Battleships < Sinatra::Base
   enable :sessions
 
+  @@turn = 0
+  PLAYER_ONE = Player.new
+  PLAYER_TWO = Player.new
+
   get '/' do
-    session[:player] = Player.new
-    session[:computer] = Player.new
-    erb :index
+    if @@turn == 0
+      session[:player] = 1
+      @@turn += 1
+      "Welcome player 1, Waiting for other player"
+    elsif @@turn == 1 && session[:player] == 1
+      "Welcome back player 1, Waiting for other player"
+    elsif @@turn == 1
+      session[:player] = 2
+      @@turn += 1
+      "You've joined a game, please wait for player 1 to place ships"
+    elsif @@turn == 2 && session[:player] == 1
+      @@turn += 1
+      erb :index
+    elsif @@turn == 3 && session[:player] == 2
+      erb :index
+    end
   end
 
-  def random_col
-   (("A".."J").to_a.sample)
-  end
-
-  def random_row
-    (("1".."10").to_a.sample)
-  end
 
   post '/placed' do
-    session[:player].place(Ship, (params[:place_ship_1].upcase), :east)
-    session[:player].place(Ship, (params[:place_ship_2].upcase), :east)
-    session[:player].place(Ship, (params[:place_ship_3].upcase), :east)
-    session[:computer].place(Ship, (random_col + random_row).upcase, :east)
-    session[:computer].place(Ship, (random_col + random_row).upcase, :east)
-    session[:computer].place(Ship, (random_col + random_row).upcase, :east)
-    erb :placed
+    if session[:player] == 1
+      PLAYER_ONE.place(Ship, (params[:place_ship_1].upcase), :east)
+      PLAYER_ONE.place(Ship, (params[:place_ship_2].upcase), :east)
+      PLAYER_ONE.place(Ship, (params[:place_ship_3].upcase), :east)
+        erb :placed
+    elsif session[:player] == 2
+      PLAYER_TWO.place(Ship, (params[:place_ship_1].upcase), :east)
+      PLAYER_TWO.place(Ship, (params[:place_ship_2].upcase), :east)
+      PLAYER_TWO.place(Ship, (params[:place_ship_3].upcase), :east)
+        erb :placed
+    end
   end
 
   post '/fire' do
-    @computers_hit = session[:player].receive_hit(random_col + random_row)
-    if session[:computer].lost?
-      "hit! Game Over Computer Loses! <a href='/'>Play again?</a>"
-    elsif session[:player].lost?
-      "computer hit you! Game Over You Lose! <a href='/'>Play again?</a>"
-    elsif session[:computer].receive_hit(params[:to_fire_at].upcase) == :miss
+    if PLAYER_ONE.lost?
+      "Game Over PLAYER ONE Loses! <a href='/'>Play again?</a>"
+    elsif PLAYER_TWO.lost?
+      "Game Over PLAYER TWO Loses! <a href='/'>Play again?</a>"
+    elsif session[:player] == 1 &&
+      PLAYER_TWO.receive_hit(params[:to_fire_at].upcase) == :miss
+      erb :missed
+    elsif session[:player] == 2 &&
+      PLAYER_ONE.receive_hit(params[:to_fire_at].upcase) == :miss
       erb :missed
     else
       erb :hit
